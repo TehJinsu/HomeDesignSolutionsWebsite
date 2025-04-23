84055ba46195b79f9c96b80a99f6efbeb0ec7529
TehJinsu <112648902+TehJinsu@users.noreply.github.com>
2025-04-22 22:28:29 -0400

    Pretty Stuff
    making the website more beautiful

--- js/main.js
+++ js/main.js
@@ -1,7 +1,35 @@
+//opens the popup for the login
 function openLogin() {
     document.getElementById('loginPopup').style.visibility = "visible";
 }
-
+//closes the popup
 function closeLogin() {
     document.getElementById("loginPopup").style.visibility = "hidden";
 }
+
+//prints the div element within the selected area
+function printReceipt() {
+    const printContents = document.getElementById('printReceipt').innerHTML;
+    const originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+    window.print();
+    document.body.innerHTML = originalContents;
+}
+
+//displays and hides the selected tab
+const buttons = document.querySelectorAll('.button-tab');
+const contents = document.querySelectorAll('.tab-content');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+
+    buttons.forEach(btn => btn.classList.remove('active'));
+    contents.forEach(content => content.classList.remove('active'));
+
+
+    button.classList.add('active');
+    const tabId = button.getAttribute('data-tab');
+    document.getElementById(tabId).classList.add('active');
+  });
+});
